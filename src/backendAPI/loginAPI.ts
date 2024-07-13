c9ef0308beb8d0d868aa937ff008bf20d6772df5
PeterMagPeter <petermagpeter546@gmail.com>
2024-07-13 20:30:13 +0200

    test

--- src/backendAPI/loginAPI.ts
+++ src/backendAPI/loginAPI.ts
@@ -3,7 +3,7 @@ export async function postLogin(
   email: string,
   password: string
 ): Promise<LoginResource> {
-  const url = `/api/login`;
+  const url = `${process.env.REACT_APP_API_SERVER_URL}/api/login`;
   console.log("email: " + email+ "pw: "+password)
   const request = {
     method: "POST",
@@ -21,14 +21,14 @@ export async function postLogin(
 }
 
 export async function getLogin(): Promise<LoginResource | false> {
-  const url = `/api/login`;
+  const url = `${process.env.REACT_APP_API_SERVER_URL}/api/login`;
   const request = { credentials: "include" as RequestCredentials };
   const response = await fetch(url, request);
   return await response.json();
 }
 
 export async function deleteLogin() {
-  const url = `/api/login`;
+  const url = `${process.env.REACT_APP_API_SERVER_URL}/api/login`;
   const request = {
     methode: "DELETE",
     headers: {
--- src/backendAPI/userAPI.ts
+++ src/backendAPI/userAPI.ts
@@ -1,21 +1,21 @@
 import { UserResource } from "../Resources";
 
 export async function getAllUsers(): Promise<UserResource> {
-  const url = `/api/user/all`;
+  const url = `${process.env.REACT_APP_API_SERVER_URL}/api/user/all`;
   const request = { credentials: "include" as RequestCredentials };
   const response = await fetch(url, request);
   return await response.json();
 }
 
 export async function getUser(userId: string): Promise<UserResource> {
-  const url = `/api/user/${userId}`;
+  const url = `${process.env.REACT_APP_API_SERVER_URL}/api/user/${userId}`;
   const request = { credentials: "include" as RequestCredentials };
   const response = await fetch(url, request);
   return await response.json();
 }
 
 export async function postUser(user: UserResource): Promise<UserResource> {
-  const url = `/api/user`;
+  const url = `${process.env.REACT_APP_API_SERVER_URL}/api/user`;
   const request = {
     method: "POST",
     headers: {
@@ -29,7 +29,7 @@ export async function postUser(user: UserResource): Promise<UserResource> {
 }
 
 export async function putUser(user: UserResource): Promise<UserResource> {
-  const url = `/api/user/${user.id}`;
+  const url = `${process.env.REACT_APP_API_SERVER_URL}/api/user/${user.id}`;
   const request = {
     method: "PUT",
     headers: {
@@ -43,7 +43,7 @@ export async function putUser(user: UserResource): Promise<UserResource> {
 }
 
 export async function deleteUser(userId: string) {
-  const url = `/api/user/${userId}`;
+  const url = `${process.env.REACT_APP_API_SERVER_URL}/api/user/${userId}`;
   const request = {
     method: "DELETE",
     headers: {
